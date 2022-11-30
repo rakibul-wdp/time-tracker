5d4da1e20728c7fd3f5ab1c1934b7ad3e4d5b243
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-01 05:25:20 +0600

    create main style or index

---
+++ src/Components/section/Home/styles/index.tsx
@@ -0,0 +1,7 @@
+export * from "./CardHeaderMain";
+export * from "./CardBodyMain";
+export * from "./CardArea";
+export * from "./CardFooterMain";
+export * from "./FooterBody";
+export * from "./Select";
+export * from "./DropdownList";
