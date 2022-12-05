4e81a3cca39f355d7e1954ba6087f1a43a10f105
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-06 04:25:20 +0600

    create common label style

---
+++ src/Components/common/Label.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Label = styled.label`
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+    color: #868e96;
+`;
