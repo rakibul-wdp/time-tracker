c7ed0b3e1e3f92ab3731f8643db3344bd3c48dc7
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-10 04:05:20 +0600

    create select style

---
+++ src/Components/section/Home/styles/Select.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Select = styled.div`
+    width: 134px;
+    color: #2da771;
+    background-color: #ffffff;
+    border: 1px solid #32a071;
+    padding: 10px 0;
+    border-radius: 10px;
+    cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 25px;
+    margin-top: 5px;
+`;
